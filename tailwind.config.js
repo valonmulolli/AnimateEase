d65e59f4d15069456966a2fd72e41fae394d9f15
valonmulolli <valonmulolli@gmail.com>
2024-03-16 19:23:00 +0100

    add nativewind and reanimated

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './_App.{js,jsx,ts,tsx}',
+    './src/screens/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
